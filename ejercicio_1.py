faad66af56a84a7c2a8018851adb1d06995d070b
Ulises <ulisesgroup@gmail.com>
2026-05-12 20:35:14 -0300

    feat(tp2): ejecicio_1 resuelto

---
+++ ejercicio_1.py
@@ -0,0 +1,7 @@
+﻿print(".: Verificador de Mayúsculas Capital de letra P :.")
+print()
+
+palabra = input("Ingrese una palabra: ")
+
+verificacion = lambda palabra : palabra.startswith("P")
+print(verificacion(palabra))
